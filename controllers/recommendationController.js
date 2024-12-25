0666c521b8e1421dd600989cbdb5a9e627e018b4
amirulkanak <coder.kanak@gmail.com>
2024-12-25 16:13:56 +0600

    get Al lRecommendation By queryId routes

--- controllers/recommendationController.js
+++ controllers/recommendationController.js
@@ -36,6 +36,40 @@ export const getAllRecommendation = async (req, res) => {
   }
 };
 
+// Get all Recommendation data by queryId
+export const getAllRecommendationByQueryId = async (req, res) => {
+  try {
+    const db = await connectDB();
+    const collection = db.collection(collectionName);
+    const result = await collection
+      .find({ queryId: req.params.queryId })
+      .sort({ createdAt: -1 })
+      .toArray();
+    if (result.length === 0) {
+      res.status(200).json({
+        success: true,
+        message: 'No Recommendation data found',
+        result,
+      });
+    } else {
+      res.status(200).json({
+        success: true,
+        message: 'Recommendation data retrieved successfully',
+        result,
+      });
+    }
+  } catch (error) {
+    res.status(500).json({
+      success: false,
+      message: 'Failed to retrieve Recommendation data',
+      error: {
+        code: 'INTERNAL_SERVER_ERROR',
+        details: error.message,
+      },
+    });
+  }
+};
+
 // Post a new Recommendation data
 export const postRecommendation = async (req, res) => {
   // Update queries collection recommendationCount
--- routes/recommendationRoutes.js
+++ routes/recommendationRoutes.js
@@ -1,6 +1,7 @@
 import express from 'express';
 import {
   getAllRecommendation,
+  getAllRecommendationByQueryId,
   postRecommendation,
 } from '../controllers/recommendationController.js';
 
@@ -9,6 +10,9 @@ const recommendationRoutes = () => {
 
   // GET all /recommendation/all
   route.get('/all', getAllRecommendation);
+
+  // Get all Recommendation data by queryId
+  route.get('/all/:queryId', getAllRecommendationByQueryId);
 
   // POST one /recommendation/create
   route.post('/create', postRecommendation);
